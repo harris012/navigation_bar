d131ba819ccef2cda8c791d0c37e54b7aabaa6a6
harris012 <harissaeed39@yahoo.com>
2021-04-30 08:03:20 +0200

    Updates

---
+++ precache-manifest.533f79dd814fd0ad9c6adbcb030ac18b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d431e9eb857341075adbc97d413a362c",
+    "url": "/navigation_bar/index.html"
+  },
+  {
+    "revision": "114abd6e327ad7da5b61",
+    "url": "/navigation_bar/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/navigation_bar/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navigation_bar/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114abd6e327ad7da5b61",
+    "url": "/navigation_bar/static/js/main.80572cba.chunk.js"
+  },
+  {
+    "revision": "44eec419f0761b79536d",
+    "url": "/navigation_bar/static/js/runtime-main.b410e1ae.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/navigation_bar/static/media/logo.2bb7da65.svg"
+  }
+]);
